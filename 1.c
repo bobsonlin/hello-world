407dd43ab9ce80fc88f23b017eeec8e89a9ffcb9
yoyo0 <b890052@gmail.com>
2017-07-07 13:32:53 +0800

    Wrap the pintName into a function

--- 1.c
+++ 1.c
@@ -1,12 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void printName();
+
 int main ()
+{
+    void printName();
+    printf("hello world!\n");
+
+    return 0;
+}
+
+
+void printName()
 {
     char name[20] = {0};
     printf("Type your name:");
     scanf("%s", name);
-    printf("hello world!\n");
-
-    return 0;
 }
